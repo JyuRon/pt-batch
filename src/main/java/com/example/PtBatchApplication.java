73c16d0bd164f7e994496e94f6c88d29d051794b
Jyuka <yoyksks3@gmail.com>
2023-04-10 18:32:07 +0900

    SpringBatch 의존성 동작 확인 테스트

--- src/main/java/com/example/PtBatchApplication.java
+++ src/main/java/com/example/PtBatchApplication.java
@@ -1,10 +1,41 @@
 package com.example;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+@EnableBatchProcessing
 @SpringBootApplication
+@RequiredArgsConstructor
 public class PtBatchApplication {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Step passStep(){
+        return stepBuilderFactory.get("passStep")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("Execute passStep");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Job passJob(){
+        return jobBuilderFactory.get("passJob")
+                .start(passStep())
+                .build()
+                ;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PtBatchApplication.class, args);
